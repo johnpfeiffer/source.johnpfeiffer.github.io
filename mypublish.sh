1f1088d8bd486528be6e30c0e9288d7c42b1431b
john pfeiffer <me@john-pfeiffer.com>
2014-07-31 04:49:04 +0000

    improved scripts for one line publish of pelican

--- mypublish.sh
+++ mypublish.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-rm -rf ./output
-rm -rf ./cache
-rm -f *.pyc
+git pull
+GITMESSAGE=$(git log -n 1)
+OUTPUT="../output-johnpfeiffer.bitbucket.org"
+./clean-output.sh "../output-johnpfeiffer.bitbucket.org"
+
 pelican content
+cp -a ./output/* ../output-johnpfeiffer.bitbucket.org
 
-SOURCE=../*
-for F in $SOURCE; do
-  if [ -d "$F" ] && [ "$F" == "../BLOG" ] ; then
-    printf "$F\n\n"
-  else
-    # printf "$F\n"
-    rm -rf "$F"
-  fi
-done
-
-ls -ahl ../
-
-cp -a ./output/* ../
 rm -rf ./output
 rm -rf ./cache
 rm -f *.pyc
+
+cd "$OUTPUT"
+git add --all .
+git commit -m "source $GITMESSAGE"
+git push
