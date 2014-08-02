c4456a400819c554c8f66dcac0e37920494d33d0
john pfeiffer <me@john-pfeiffer.com>
2014-08-02 00:16:07 +0000

    flask app to run publish.sh and convert pelican blog source to output html

---
+++ webhook-publish-blog.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+from subprocess import Popen, PIPE
+
+app = Flask(__name__)
+
+@app.route('/jpblog')
+def john():
+  try:
+      # output = Popen(["pwd"], stdout=PIPE).communicate()[0]
+      output = Popen(["./mypublish.sh"], stdout=PIPE).communicate()[0]
+  except Exception as error:
+      return str(error)
+  return output
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8084, use_reloader=True)
